80a73e0b2f1bf162ed2d476835f6790ab3cd1ffe
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2021-04-14 10:46:44 -0500

    Add PokemonList component

---
+++ src/components/PokemonList/PokemonList.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import './PokemonList.css';
+import PokemonCard from '../PokemonCard/PokemonCard';
+
+const PokemonList = () => {
+  const [data, setData] = useState(null);
+  const fetchURL = "https://pokeapi.co/api/v2/pokemon?limit=30";
+  const imageURL = 'https://pokeres.bastionbot.org/images/pokemon';
+
+  const getData = async () => await fetchPokemon()
+
+  useEffect(() => {
+    getData().then((data) => setData(data))
+  }, [])
+
+  async function fetchPokemon(){
+    let pokemons = [];
+    try{
+      const response = await fetch(`${fetchURL}`)
+      const json = await response.json();
+
+      for (const item of json.results){
+        let pokemon = await fetchPokemonData(item);
+        pokemon.img = `${imageURL}/${pokemon.id}.png`;
+        pokemons.push(pokemon);
+      }
+
+      return pokemons;
+    }catch(error){
+      throw new Error(error);
+    }
+  }
+
+  async function fetchPokemonData(pokemon){
+    let url = pokemon.url;
+    try{
+      const response = await fetch(url);
+      const json = await response.json();
+      return json;
+    }catch(error){
+      throw new Error(error);
+    }
+  }
+
+    return (
+      <div className="row mt-5">
+        <div className="col-4" >
+        {data ? 
+          (data.map((item) => 
+            <PokemonCard key={item.name} 
+               name={item.name}
+               img={item.img}
+               id={item.id}
+               height={item.height}
+               weight={item.weight}
+             />
+          )) : <div className="bg-warning text-center">Loading...</div> }
+          </div>
+        </div>
+     )
+}
+
+export default PokemonList;
